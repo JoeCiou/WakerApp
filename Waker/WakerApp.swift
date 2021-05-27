51091b6bc147e93c858cb02b7ee7dc66db157d02
JoeCiou <joeciou810@gmail.com>
2021-05-27 15:18:28 +0800

    First commit

---
+++ Waker/WakerApp.swift
@@ -0,0 +1,17 @@
+//
+//  WakerApp.swift
+//  Waker
+//
+//  Created by Joe Ciou on 2021/5/27.
+//
+
+import SwiftUI
+
+@main
+struct WakerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ WakerTests/WakerTests.swift
@@ -0,0 +1,33 @@
+//
+//  WakerTests.swift
+//  WakerTests
+//
+//  Created by Joe Ciou on 2021/5/27.
+//
+
+import XCTest
+@testable import Waker
+
+class WakerTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
